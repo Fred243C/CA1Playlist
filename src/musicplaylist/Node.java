17c4369af8fa7ac328524ae30bf5e6dbce438124
Fred <114075370+Fred243C@users.noreply.github.com>
2024-03-14 11:21:44 +0000

    CA1PlayList

---
+++ src/musicplaylist/Node.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package musicplaylist;
+
+/**
+ *
+ * @author alexb
+ */
+public class Node {
+    private Songs data;
+    private Node next;
+    private Node prev;
+
+    public Node(Songs data) {
+        this.data = data;
+        this.next=null;
+        this.prev=null;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Node getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" + "element=" + data + ", next=" + next + ", prev=" + prev + '}';
+    }
+     
+     
+     
+    
+}
---
+++ src/musicplaylist/Playlist.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package musicplaylist;
+
+/**
+ *
+ * @author alexb
+ */
+public class Playlist {
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public Playlist() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+        //Add method
+        
+        
+}
+
+    public void addSongs(Song data){
+
+}
+
+
+}
+
+
