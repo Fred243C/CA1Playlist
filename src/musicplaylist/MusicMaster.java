db882a6bdc441e8604bd4b7824c88164750f0685
Fred <114075370+Fred243C@users.noreply.github.com>
2024-03-16 22:39:49 +0000

    CA1PlayList

--- src/musicplaylist/MusicMaster.java
+++ src/musicplaylist/MusicMaster.java
@@ -11,6 +11,7 @@ import java.util.HashMap;
  * @author alexb
  */
 public class MusicMaster {
+
     private PlaylistNode likedSongs;
     private HashMap<String, PlaylistNode> genrePlaylist;
 
@@ -18,54 +19,63 @@ public class MusicMaster {
         likedSongs = new PlaylistNode();
         genrePlaylist = new HashMap<>();
     }
-     
+
     // Add songs to the liked playlist
-    
-    public void addToLikedSongs(Songs songs){
+    public void addToLikedSongs(Songs songs) {
         likedSongs.add(songs);
     }
+
     //Move last added song from liked liked playlist to genre playlist
-    public void moveToGenrePlaylist(String genre){
-        Node lastSongNode= likedSongs.getLastSong();
-        if(lastSongNode !=null){
+    public void moveToGenrePlaylist(String genre) {
+        Node lastSongNode = likedSongs.getLastSong();
+        if (lastSongNode != null) {
             Songs lastSong = lastSongNode.getSongs();
-            if(genrePlaylist.containsKey(genre)){
+            if (genrePlaylist.containsKey(genre)) {
                 genrePlaylist.get(genre).add(lastSong);
-            }else{
-                PlaylistNode newGenrePlaylist= new PlaylistNode();
+            } else {
+                PlaylistNode newGenrePlaylist = new PlaylistNode();
                 newGenrePlaylist.add(lastSong);
                 genrePlaylist.put(genre, newGenrePlaylist);
-                
+
             }
             likedSongs.removeLast();
-        }else{
+        } else {
             System.out.println("List empty.");
         }
-        
+
     }
+
     //Print Playlist
-    public void printPlaylist(String name){
-        if(name.equalsIgnoreCase("liked")){
+    public void printPlaylist(String playlistName) {
+        if (playlistName.equalsIgnoreCase("liked")) {
             likedSongs.print();
-        }else if(genrePlaylist.containsKey(name)){
-            genrePlaylist.get(name).print();
-            
-        }else{
+        } else if (genrePlaylist.containsKey(playlistName)) {
+            genrePlaylist.get(playlistName).print();
+
+        } else {
             System.out.println("Not found");
         }
     }
+
     // Show number of Songs in a playlist
-    public void showNumberOfSongs(String name){
+    public void showNumberOfSongs(String playlistName) {
         int count;
-        if(name.equalsIgnoreCase("liked"));
+        if (playlistName.equalsIgnoreCase("liked"));
         count = likedSongs.size();
-        System.out.println("liked Playlist:"+count);
+        System.out.println("liked Playlist:" + count);
+
+    }else if(genrePlaylist.containsKey (playlistNode) ){
+        count = genrePlaylists.get(playlistName).size();
+        System.out.println("Number of songs in " + playlistName + " playlist: " + count);
+
+    }else{
+    System.out.println("Not found");
     }
+}
+// Create a repeatable playlist
+     public Playlist createRepeatablePlaylist(){
+      return new Playlist(true);
+     }
     
-    
-    
-    }
-    
-    
-    
+
 
